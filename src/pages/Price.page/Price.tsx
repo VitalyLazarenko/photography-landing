148cbd9519ab9359d36bf142a14ad9fc4072cb11
vitalii-lazarenko <vitalii@unilimes.com>
2020-11-02 10:25:57 +0200

    create price page, create price container

--- src/pages/Price.page/Price.tsx
+++ src/pages/Price.page/Price.tsx
@@ -9,44 +9,135 @@ export const PricePage = () => {
                 <img src="/assets/images/PricePage/photo.jpg" alt=""/>
             </Grid>
 
-            <Grid item md={10} className={styles.card_container}>
-                <Grid item md={12} className={styles.title_container}>
+            <Grid item md={8} className={styles.card_container}>
+                <Grid item md={12} className={styles.title_wrapper}>
                     <Typography className={styles.title}>Услуги</Typography>
                 </Grid>
 
-                <Grid item md={4} className={styles.card}>
-                    <section className={styles.title_container}>
-                        <Typography className={styles.title}>Пакет «Минимум»</Typography>
-                    </section>
-                    <section className={styles.description_container}>
-                        <Typography className={styles.text}>Встреча с молодоженами, консультация по всем
-                            вопросам.</Typography>
-                    </section>
-                    <section className={styles.description_container}>
-                        <Typography className={styles.text}>Фотосъемка до 15 часов. Команда из двух
-                            фотографов.</Typography>
-                    </section>
-                    <section className={styles.description_container}>
-                        <Typography className={styles.text}>750 и более обработанных фотографий в дизайнерской
-                            упаковке.</Typography>
-                    </section>
-                    <section className={styles.description_container}>
-                        <Typography className={styles.text}>
-                            В подарок деревянная коробочка с флешкой и 15
-                            напечатанных фото размером 10х15. Фото в полном размере для печати широкоформатных
-                            фотографий и уменьшенные копии всех фотографий.
-                        </Typography>
-                    </section>
-                    <section className={styles.description_container}>
-                        <Typography className={styles.text}>Фотокнига 30х30см и двe миникнижки 19х19см для
-                            родителей.</Typography>
-                    </section>
-                    <section className={styles.description_container}>
-                        <Typography className={styles.text}>Сроки:</Typography>
-                    </section>
-                    <section className={styles.description_container}>
-                        <Typography className={styles.text}>от 2х недель до 3 месяцев.</Typography>
-                    </section>
+                <Grid container className={styles.card_wrapper}>
+                    <Grid item md={4} className={styles.card}>
+                        <section className={styles.card_container}>
+                            <section className={styles.title_container}>
+                                <Typography className={styles.title_name}>Пакет</Typography>
+                                <Typography className={styles.name}>«Минимум»</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Встреча с молодоженами, консультация по всем
+                                    вопросам.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Фотосъемка до 15 часов. Команда из двух
+                                    фотографов.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>750 и более обработанных фотографий в дизайнерской
+                                    упаковке.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>
+                                    В подарок деревянная коробочка с флешкой и 15
+                                    напечатанных фото размером 10х15. Фото в полном размере для печати широкоформатных
+                                    фотографий и уменьшенные копии всех фотографий.
+                                </Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Фотокнига 30х30см и двe миникнижки 19х19см для
+                                    родителей.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Сроки:</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>от 2х недель до 3 месяцев.</Typography>
+                            </section>
+
+                            <section className={styles.description_container}>
+                                <button>Позвонить</button>
+                            </section>
+                        </section>
+                    </Grid>
+                    <Grid item md={4} className={styles.card}>
+                        <section className={styles.card_container}>
+                            <section className={styles.title_container}>
+                                <Typography className={styles.title_name}>Пакет</Typography>
+                                <Typography className={styles.name}>«Минимум»</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Встреча с молодоженами, консультация по всем
+                                    вопросам.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Фотосъемка до 15 часов. Команда из двух
+                                    фотографов.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>750 и более обработанных фотографий в дизайнерской
+                                    упаковке.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>
+                                    В подарок деревянная коробочка с флешкой и 15
+                                    напечатанных фото размером 10х15. Фото в полном размере для печати широкоформатных
+                                    фотографий и уменьшенные копии всех фотографий.
+                                </Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Фотокнига 30х30см и двe миникнижки 19х19см для
+                                    родителей.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Сроки:</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>от 2х недель до 3 месяцев.</Typography>
+                            </section>
+
+                            <section className={styles.description_container}>
+                                <button>Позвонить</button>
+                            </section>
+                        </section>
+                    </Grid>
+                    <Grid item md={4} className={styles.card}>
+                        <section className={styles.card_container}>
+                            <section className={styles.title_container}>
+                                <Typography className={styles.title_name}>Пакет</Typography>
+                                <Typography className={styles.name}>«Минимум»</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Встреча с молодоженами, консультация по всем
+                                    вопросам.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Фотосъемка до 15 часов. Команда из двух
+                                    фотографов.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>750 и более обработанных фотографий в дизайнерской
+                                    упаковке.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>
+                                    В подарок деревянная коробочка с флешкой и 15
+                                    напечатанных фото размером 10х15. Фото в полном размере для печати широкоформатных
+                                    фотографий и уменьшенные копии всех фотографий.
+                                </Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Фотокнига 30х30см и двe миникнижки 19х19см для
+                                    родителей.</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>Сроки:</Typography>
+                            </section>
+                            <section className={styles.description_container}>
+                                <Typography className={styles.text}>от 2х недель до 3 месяцев.</Typography>
+                            </section>
+
+                            <section className={styles.description_container}>
+                                <button>Позвонить</button>
+                            </section>
+                        </section>
+                    </Grid>
                 </Grid>
             </Grid>
         </Grid>
